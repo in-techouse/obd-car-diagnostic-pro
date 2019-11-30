1f765c98165be3aa5b5bf2eeddcf84ebb996264b
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2019-11-30 14:34:06 +0500

    Dashboard activity added

--- app/src/main/java/lcwu/fyp/obdcardiagnosticpro/Dashboard.java
+++ app/src/main/java/lcwu/fyp/obdcardiagnosticpro/Dashboard.java
@@ -2,13 +2,36 @@ package lcwu.fyp.obdcardiagnosticpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class Dashboard extends AppCompatActivity {
 
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        listView = findViewById(R.id.listView);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        List<String> s = new ArrayList<String>();
+        for(BluetoothDevice bt : pairedDevices) {
+            s.add(bt.getName());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.bluetooth_item, s);
+        listView.setAdapter(adapter);
     }
 }
