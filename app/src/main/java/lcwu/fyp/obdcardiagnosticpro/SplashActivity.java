ce11a18db913adcc9532ae4f95eb14db0635f51b
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2019-12-14 14:33:19 +0500

    Working Splash Screen

--- app/src/main/java/lcwu/fyp/obdcardiagnosticpro/SplashActivity.java
+++ app/src/main/java/lcwu/fyp/obdcardiagnosticpro/SplashActivity.java
@@ -2,13 +2,56 @@ package lcwu.fyp.obdcardiagnosticpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView application_logo;
+    TextView application_name;
+    Animation upToDown, downToUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        application_logo = findViewById(R.id.application_logo);
+        application_name = findViewById(R.id.application_name);
+
+        upToDown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        downToUp = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+
+
+        application_logo.setAnimation(upToDown);
+        application_name.setAnimation(downToUp);
+
+        RotateAnimation rotate = new RotateAnimation(0, 720, Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(3000);
+        rotate.setInterpolator(new LinearInterpolator());
+        application_logo.startAnimation(rotate);
+
+
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(),Dashboard.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }
