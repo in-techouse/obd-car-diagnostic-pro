d3b9e28c33c9fd4080a4393ae09f18f764e8e217
Mahnoor <naziashahid679@gmail.com>
2020-01-04 13:51:49 +0500

    working on info dialog

--- app/src/main/java/lcwu/fyp/obdcardiagnosticpro/dialogue_box/InfoDialogue.java
+++ app/src/main/java/lcwu/fyp/obdcardiagnosticpro/dialogue_box/InfoDialogue.java
@@ -1,4 +1,29 @@
 package lcwu.fyp.obdcardiagnosticpro.dialogue_box;
 
-public class InfoDialogue {
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import androidx.annotation.NonNull;
+
+import lcwu.fyp.obdcardiagnosticpro.R;
+
+public class InfoDialogue extends Dialog implements View.OnClickListener {
+    public InfoDialogue(@NonNull Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.info_dialoguebox);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
