c2889cde8331956d939ddd5e5b1f79c25e19c602
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-04-28 08:37:30 +0500

    Working on acceleration test

---
+++ app/src/main/java/lcwu/fyp/obdcardiagnosticpro/AccelerationTest.java
@@ -0,0 +1,14 @@
+package lcwu.fyp.obdcardiagnosticpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccelerationTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceleration_test);
+    }
+}
